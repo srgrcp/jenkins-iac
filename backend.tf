0ae6fbf1f9522adbdd0876ca9a46de0a26a02f83
Sergio Carrillo <srgrcp@gmail.com>
2024-02-06 21:44:27 -0500

    Update backend.tf with new resource group and storage account names

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "terraformsasrgrcp"
-    container_name       = "jenkins-tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate74ong"
+    container_name       = "tfstate"
+    key                  = "jenkins.tfstate"
   }
 }
